85d40570d880923248f004975838b8c9df1def6f
Subikesh <subikeshps@gmail.com>
2021-08-23 23:49:30 +0530

    Refactored session from domain to data module

--- data/src/main/java/com/example/data/session/SessionManager.kt
+++ data/src/main/java/com/example/data/session/SessionManager.kt
@@ -1,4 +1,4 @@
-package com.example.domain.session
+package com.example.data.session
 
 import android.content.Context
 import android.content.SharedPreferences
@@ -6,18 +6,15 @@ import android.content.SharedPreferences
 class SessionManager(val _context: Context) {
     var preferences: SharedPreferences = _context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
     var editor: SharedPreferences.Editor = preferences.edit()
+    var login: Boolean
+        get() = preferences.getBoolean(KEY_LOGGED_IN, false)
+        set(value) {
+            editor.putBoolean(KEY_LOGGED_IN, value)
+            editor.commit()
+        }
 
     companion object {
         private val PREF_NAME: String = "EcommerceSession"
         private val KEY_LOGGED_IN: String = "isLoggedin"
-    }
-
-    fun setLogin(isLoggedIn: Boolean) {
-        editor.putBoolean(KEY_LOGGED_IN, isLoggedIn)
-        editor.commit()
-    }
-
-    fun isLoggedIn(): Boolean {
-        return preferences.getBoolean(KEY_LOGGED_IN, false)
     }
 }

1e68c8214b28ee1dcfe5fa17af334ecfa9666007
Subikesh <subikeshps@gmail.com>
2021-08-28 21:56:14 +0530

    User sign up function implemented

--- data/src/main/java/com/example/data/usecases/Authentication.kt
+++ data/src/main/java/com/example/data/usecases/Authentication.kt
@@ -1,10 +1,9 @@
 package com.example.data.usecases
 
 import android.content.Context
-import androidx.room.Room
+import android.database.sqlite.SQLiteException
+import android.util.Log
 import com.example.data.db.DatabaseContract
-import com.example.data.db.EcommerceContract
-import com.example.data.db.EcommerceDatabase
 import com.example.data.entities.User
 import com.example.data.session.SessionManager
 
@@ -17,17 +16,44 @@ class Authentication(context: Context) {
 
     /**
      * Login user using user mail and password
-     * @param email User's mail id
-     * @param password User password
+     * @param email     User's mail id
+     * @param password  User password
      * @return the user entity who is logged in
      */
     suspend fun userLogin(email: String, password: String): User {
         val user = db.userDao().findLogin(email, password)
-        if (user != null) {
-            session.login = true
-            session.user = user
-        }
+        userLogin(user)
+        Log.i("LoginFragment", "userLogin: $user logged in")
         return user
+    }
+
+    /**
+     * Utility function to login with user entity
+     * @param user  User entity object to login
+     */
+    private fun userLogin(user: User) {
+        session.login = true
+        session.user = user
+    }
+
+    /**
+     * Create new user if email is not already present
+     * @param email     user's email
+     * @param password  user's password
+     * @param username  user's username
+     * @return newly created user if successful, else null
+     */
+    suspend fun userSignup(email: String, password: String, username: String): User? {
+        val insertUser = User(username = username, password = password, email = email)
+        var returnCode: Long? = null
+        try {
+            returnCode = db.userDao().insert(insertUser)
+            Log.d("LoginFragment", "userSignup: user insert return code: $returnCode")
+            userLogin(insertUser)
+            return insertUser
+        } catch (e: SQLiteException) {
+            return null
+        }
     }
 
     /**

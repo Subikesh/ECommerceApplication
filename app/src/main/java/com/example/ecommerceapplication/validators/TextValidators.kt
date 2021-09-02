1f7f3104c2cdcf7e8c3efc7637b347ab81943fc6
Subikesh <subikeshps@gmail.com>
2021-09-02 10:27:39 +0530

    Password error text edited

--- app/src/main/java/com/example/ecommerceapplication/validators/TextValidators.kt
+++ app/src/main/java/com/example/ecommerceapplication/validators/TextValidators.kt
@@ -1,5 +1,7 @@
 package com.example.ecommerceapplication.validators
 
+import android.os.Build
+import android.text.Html
 import android.widget.EditText
 import java.util.regex.Pattern
 
@@ -9,10 +11,15 @@ object TextValidators {
      * Password validation regex
      */
     private const val PASSWORD_VALIDATOR = "^(?=.*[0-9])(?=.*[A-Za-z])(?=\\S+\$).{8,}\$"
+    private const val PASSWORD_ERROR_TEXT = "Password must have <ul>" +
+            "<li> at least 8 characters</li>" +
+            "<li> an alphabet</li>" +
+            "<li> a number</li>"
+    private const val EMAIL_ERROR_TEXT = "Please enter a valid email address!"
 
     /**
      * Checks if the email entered is valid or not
-     * @param mailText  The edit text of the entered email
+     * @param mailText The edit text of the entered email
      * @return true if mail is valid, else false
      */
     fun checkEmail(mailText: EditText): Boolean {
@@ -22,7 +29,7 @@ object TextValidators {
             mailText.error = "Please enter an email address!"
             error = false
         } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            mailText.error = "Please enter a valid email address!"
+            mailText.error = EMAIL_ERROR_TEXT
             error = false
         }
         if (error)
@@ -32,7 +39,7 @@ object TextValidators {
 
     /**
      * Checks if the password entered is valid or not based on the regex PASSWORD_VALIDATOR
-     * @param mailText  The edit text of the entered email
+     * @param passwordText The edit text of the entered email
      * @return true if mail is valid, else false
      */
     fun checkPassword(passwordText: EditText): Boolean {
@@ -43,7 +50,12 @@ object TextValidators {
             passwordText.error = "Please enter password!"
             error = false
         } else if (!pattern.matcher(password).matches()) {
-            passwordText.error = "Password must have 8 characters and have at least an alphabet and a number"
+            passwordText.error = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) Html.fromHtml(
+                PASSWORD_ERROR_TEXT,
+                Html.FROM_HTML_MODE_COMPACT
+            ) else Html.fromHtml(
+                PASSWORD_ERROR_TEXT
+            )
             error = false
         }
         if (error)

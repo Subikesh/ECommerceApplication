094fac9bf640bf9066bc803b62b3318a70e43600
subikeshps <subikeshps@gmail.com>
2022-07-08 15:15:40 +0530

    Added room module

---
+++ data/src/main/java/com/example/data/di/RoomModule.kt
@@ -0,0 +1,54 @@
+package com.example.data.di
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.data.roomdb.EcommerceDatabase
+import com.example.data.roomdb.dao.ProductDao
+import com.example.data.roomdb.dao.ShoppingCartDao
+import com.example.data.roomdb.dao.UserDao
+import com.example.data.roomdb.dao.WishlistDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RoomModule(context: Context) {
+    private val database: EcommerceDatabase = Room.databaseBuilder(context, EcommerceDatabase::class.java, "demo-db").build()
+
+    @Singleton
+    @Provides
+    fun providesRoomDatabase(): EcommerceDatabase {
+        return database
+    }
+
+    @Singleton
+    @Provides
+    fun providesUserDao(database: EcommerceDatabase): UserDao {
+        return database.userDao()
+    }
+
+    @Singleton
+    @Provides
+    fun providesProductDao(database: EcommerceDatabase): ProductDao {
+        return database.productDao()
+    }
+
+    @Singleton
+    @Provides
+    fun providesWishlistDao(database: EcommerceDatabase): WishlistDao {
+        return database.wishlistDao()
+    }
+
+    @Singleton
+    @Provides
+    fun providesShoppingCartDao(database: EcommerceDatabase): ShoppingCartDao {
+        return database.cartDao()
+    }
+
+//    @Singleton
+//    @Provides
+//    fun productRepository(productDao: ProductDao?): ProductRepository {
+//        return ProductDataSource(productDao)
+//    }
+}

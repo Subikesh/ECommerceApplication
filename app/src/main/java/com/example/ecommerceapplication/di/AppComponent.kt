bcac560e06caab3b16fbffa0b9f09a609abc4ffd
subikeshps <subikeshps@gmail.com>
2022-07-08 17:58:24 +0530

    App component injection fragments added

--- app/src/main/java/com/example/ecommerceapplication/di/AppComponent.kt
+++ app/src/main/java/com/example/ecommerceapplication/di/AppComponent.kt
@@ -2,14 +2,16 @@ package com.example.ecommerceapplication.di
 
 import com.example.data.di.PreferenceModule
 import com.example.data.di.RoomModule
-import com.example.ecommerceapplication.ui.user.LoginFragment
-import com.example.ecommerceapplication.ui.user.UserFragment
+import com.example.ecommerceapplication.ui.user.*
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [PreferenceModule::class, AppModule::class, RoomModule::class])
 interface AppComponent {
-    fun inject(activity: UserFragment)
-    fun inject(activity: LoginFragment)
+    fun inject(fragment: UserFragment)
+    fun inject(fragment: LoginFragment)
+    fun inject(fragment: SignupFragment)
+    fun inject(fragment: ProfileFragment)
+    fun inject(fragment: WishlistFragment)
 }

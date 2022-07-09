a37293ac10c55363479720bad2c501aff08da280
subikeshps <subikeshps@gmail.com>
2022-07-10 00:03:06 +0530

    Created a toast util

---
+++ app/src/main/java/com/example/ecommerceapplication/util/ToastUtil.kt
@@ -0,0 +1,18 @@
+package com.example.ecommerceapplication.util
+
+import android.content.Context
+import android.widget.Toast
+import javax.inject.Inject
+
+/** Contains constants regrading toast durations */
+sealed class ToastDuration(val value: Int) {
+    object SHORT : ToastDuration(Toast.LENGTH_SHORT)
+    object LONG : ToastDuration(Toast.LENGTH_LONG)
+}
+
+/** Util functions regarding toasts */
+class ToastUtil @Inject constructor(private val context: Context) {
+    fun displayToast(message: String, duration: ToastDuration) {
+        Toast.makeText(context, message, duration.value).show()
+    }
+}
--- data/src/main/java/com/example/data/usecases/UserOrders.kt
+++ data/src/main/java/com/example/data/usecases/UserOrders.kt
@@ -1,44 +1,48 @@
 package com.example.data.usecases
 
-import android.content.Context
 import com.example.data.repository.ProductEntityMapperImpl
 import com.example.data.repository.UserEntityMapperImpl
-import com.example.data.roomdb.DatabaseContract
+import com.example.data.roomdb.dao.OrderDao
+import com.example.data.roomdb.dao.ProductDao
+import com.example.data.roomdb.dao.ShoppingCartDao
 import com.example.data.roomdb.entities.Order
 import com.example.data.roomdb.entities.ShoppingCart
 import com.example.data.roomdb.relations.OrderWithCartItems
 import com.example.domain.models.Product
 import com.example.domain.models.User
+import javax.inject.Inject
 
-class UserOrders(context: Context) {
-    /** Database object to access database */
-    private val db = DatabaseContract.getInstance(context)
+class UserOrders @Inject constructor(
+    private val cartDao: ShoppingCartDao,
+    private val productDao: ProductDao,
+    private val orderDao: OrderDao
+) {
 
     /** Remove user's shopping cart and transferred it to order history */
     suspend fun moveCartToOrder(_user: User, successful: Boolean = true) {
         val user = UserEntityMapperImpl.toEntity(_user)
-        val userCart = db.cartDao().getShoppingCart(user.userId)
-        db.cartDao().deleteShoppingCart(userCart.cart)
+        val userCart = cartDao.getShoppingCart(user.userId)
+        cartDao.deleteShoppingCart(userCart.cart)
         buyCart(userCart.cart, successful)
         // Creating new shopping cart for user
-        db.cartDao().addShoppingCart(ShoppingCart(userId = user.userId))
+        cartDao.addShoppingCart(ShoppingCart(userId = user.userId))
     }
 
     /** Add a single product to orders */
     suspend fun buyProduct(_user: User, _product: Product): ShoppingCart {
         val user = UserEntityMapperImpl.toEntity(_user)
         val product = ProductEntityMapperImpl.toEntity(_product)
-        db.productDao().insert(product)
-        return db.cartDao().buyProductForUser(user, product)
+        productDao.insert(product)
+        return cartDao.buyProductForUser(user, product)
     }
 
     /** Get the list of products saved in user's shopping cart */
     suspend fun getCartItem(user: User): List<OrderWithCartItems> {
-        return db.orderDao().getOrderCartsForUser(user.userId)
+        return orderDao.getOrderCartsForUser(user.userId)
     }
 
     suspend fun buyCart(cart: ShoppingCart, isSuccessful: Boolean = true) =
-        db.orderDao().addOrder(Order(cart.cartId, cart.userId, cart.total, isSuccessful))
+        orderDao.addOrder(Order(cart.cartId, cart.userId, cart.total, isSuccessful))
 
-    suspend fun getProduct(productId: String) = db.productDao().getProduct(productId)
+    suspend fun getProduct(productId: String) = productDao.getProduct(productId)
 }

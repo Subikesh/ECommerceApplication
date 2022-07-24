8a2eb7c0ac222ccff829850af4dcb0ac46640b23
subikeshps <subikeshps@gmail.com>
2022-07-24 11:43:33 +0530

    Created domain repository interface and implementation for wishlist

--- data/src/main/java/com/example/data/repository/WishlistRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/WishlistRepositoryImpl.kt
@@ -6,24 +6,25 @@ import com.example.data.roomdb.dao.WishlistDao
 import com.example.data.roomdb.entities.UserProductCrossRef
 import com.example.domain.models.Product
 import com.example.domain.models.User
+import com.example.domain.repository.WishlistRepository
 import javax.inject.Inject
 
-class UserWishlist @Inject constructor(
+class WishlistRepositoryImpl @Inject constructor(
     private val productDao: ProductDao,
     private val wishlistDao: WishlistDao
-) {
+) : WishlistRepository {
     /** Add product as wishlist for the user */
-    suspend fun addWishlist(user: User, product: Product) {
+    override suspend fun addWishlist(user: User, product: Product) {
         productDao.insert(ProductEntityMapperImpl.toEntity(product))
         wishlistDao.addWishlist(UserProductCrossRef(user.userId, product.productId))
     }
 
-    suspend fun removeWishlist(user: User, product: Product) {
+    override suspend fun removeWishlist(user: User, product: Product) {
         wishlistDao.removeWishlist(UserProductCrossRef(user.userId, product.productId))
     }
 
     /** Get the list of products saved in user's wishlist */
-    suspend fun getWishlist(user: User): List<Product> {
+    override suspend fun getWishlist(user: User): List<Product> {
         val products = wishlistDao.getWishlist(user.userId).products
 
         val productModels = mutableListOf<Product>()
@@ -32,7 +33,7 @@ class UserWishlist @Inject constructor(
     }
 
     /** Check if this product is already in user's wishlist */
-    suspend fun isInWishlist(user: User, product: Product): Boolean {
+    override suspend fun isInWishlist(user: User, product: Product): Boolean {
         val userProductRef = wishlistDao.findWishlist(user.userId, product.productId)
         return userProductRef != null
     }

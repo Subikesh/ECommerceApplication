90a55069123671cd5ccd9d9b6a48718269604c20
subikeshps <subikeshps@gmail.com>
2022-07-31 11:33:23 +0530

    Product fragments migrated to hilt

--- app/src/main/java/com/example/ecommerceapplication/ui/product/ProductFragment.kt
+++ app/src/main/java/com/example/ecommerceapplication/ui/product/ProductFragment.kt
@@ -16,26 +16,25 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.fragment.findNavController
-import com.example.data.di.RoomModule
-import com.example.data.session.SessionManager
 import com.example.data.repository.UserOrders
 import com.example.data.repository.UserShoppingCart
 import com.example.data.repository.UserWishlist
+import com.example.data.session.SessionManager
 import com.example.domain.models.Product
 import com.example.ecommerceapplication.MainActivity
 import com.example.ecommerceapplication.R
 import com.example.ecommerceapplication.databinding.FragmentProductBinding
-import com.example.ecommerceapplication.di.AppModule
-import com.example.ecommerceapplication.di.DaggerAppComponent
 import com.example.ecommerceapplication.extensions.NotificationChannels
 import com.example.ecommerceapplication.extensions.getGlideImage
 import com.example.ecommerceapplication.ui.cart.SHOPPING_CART
 import com.example.ecommerceapplication.util.ToastUtil
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 const val PRODUCT_OBJECT = "productObject"
 
+@AndroidEntryPoint
 class ProductFragment : Fragment() {
 
     @Inject
@@ -59,10 +58,6 @@ class ProductFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        DaggerAppComponent.builder()
-            .appModule(AppModule(requireActivity()))
-            .roomModule(RoomModule(requireActivity()))
-            .build().inject(this)
 
         val factory = ProductViewModel.Factory(session, userWishlist, userShoppingCart, userOrders, toastUtil)
         viewModel = ViewModelProvider(this, factory).get(ProductViewModel::class.java)

cb8cd5ca6039be95426a1cf1cefc97f14ddd3725
subikeshps <subikeshps@gmail.com>
2022-10-22 23:00:57 +0530

    Repository returns wrapper and helper function implemented in base repository

--- data/src/main/java/com/example/data/repository/ProductsRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/ProductsRepositoryImpl.kt
@@ -4,21 +4,39 @@ import android.util.Log
 import com.example.data.api.ApiDataService
 import com.example.data.mapper.ProductApiMapperImpl
 import com.example.data.mapper.ProductEntityMapperImpl
+import com.example.data.parser.ProductsApiParser
+import com.example.data.repository.base.BaseRepository
 import com.example.data.roomdb.dao.ProductDao
 import com.example.domain.models.Product
+import com.example.domain.repository.ProductsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class ProductsRepositoryImpl @Inject constructor(private val apiService: ApiDataService, private val productDao: ProductDao) {
-    suspend fun getProducts(productUrl: String, categoryId: String, itemCount: Int): List<Product> = withContext(Dispatchers.IO) {
-        val products = apiService.getProductsFromUrl(productUrl)
-        val productList = ProductApiMapperImpl.fromApiModel(products.body()!!, categoryId, itemCount)
-        //TODO: write products to database
-        productDao.insertAll(productList.map {
-            ProductEntityMapperImpl.toEntity(it)
-        })
-        Log.d("Prod", "Products: $products $productList")
-        return@withContext productList
-    }
+class ProductsRepositoryImpl @Inject constructor(
+    private val apiService: ApiDataService,
+    private val productsApiParser: ProductsApiParser,
+    private val productDao: ProductDao
+) : ProductsRepository, BaseRepository() {
+
+    override suspend fun getProducts(productUrl: String, categoryId: String, itemCount: Int): Result<List<Product>> =
+        withContext(Dispatchers.IO) {
+            Log.d("Products", "Products url: $productUrl $itemCount")
+            val products = executeNetworkService {
+                apiService.getProductsFromUrl(productUrl)
+            }
+            if (products.isSuccess) {
+                val productList = productsApiParser.getProducts(products.getOrNull()!!, categoryId)
+                //TODO: write products to database
+                productDao.insertAll(productList.map {
+                    ProductEntityMapperImpl.toEntity(it)
+                })
+                Log.i("Response-getProducts", "$products ")
+                Log.i("Result-getProducts", "$productList")
+                Result.success(productList)
+            } else {
+                Result.failure(products.exceptionOrNull()!!)
+            }
+//            val productList = ProductApiMapperImpl.fromApiModel(products.body()!!, categoryId, itemCount)
+        }
 }
---
+++ data/src/main/java/com/example/data/repository/base/BaseRepository.kt
@@ -0,0 +1,29 @@
+package com.example.data.repository.base
+
+import com.example.data.api.EmptyBodyException
+import com.example.data.api.NoNetworkException
+import retrofit2.Response
+import java.net.SocketTimeoutException
+
+open class BaseRepository {
+
+    suspend fun <T> executeNetworkService(execute: suspend () -> Response<T>): Result<T> {
+        return try {
+            val apiResult = execute()
+            if (apiResult.isSuccessful) {
+                val body = apiResult.body()
+                if (body != null) {
+                    Result.success(body)
+                } else {
+                    Result.failure(EmptyBodyException("Response body empty!"))
+                }
+            } else {
+                Result.failure(Exception(apiResult.message() ?: "Unknown error occurred in API response."))
+            }
+        } catch (socketException: SocketTimeoutException) {
+            Result.failure(socketException)
+        } catch (exception: Exception) {
+            Result.failure(NoNetworkException(throwable = exception))
+        }
+    }
+}

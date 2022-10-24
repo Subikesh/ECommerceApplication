5f4803dced3e694ec5c2f821405d1088793de211
subikeshps <subikeshps@gmail.com>
2022-10-24 09:55:59 +0530

    Called usecase from view model

--- app/src/main/java/com/example/ecommerceapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/ecommerceapplication/ui/home/HomeViewModel.kt
@@ -9,6 +9,7 @@ import com.example.data.repository.GetCategories
 import com.example.data.roomdb.entities.MutablePair
 import com.example.domain.models.Category
 import com.example.domain.models.Product
+import com.example.domain.usecase.GetAndSaveCategoriesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,7 +17,8 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val categoryApi: GetCategories,
-    private val categoryDatabase: CategoryDatabase
+    private val categoryDatabase: CategoryDatabase,
+    private val getAndSaveCategoriesUseCase: GetAndSaveCategoriesUseCase
 ) : ViewModel() {
 
     var categoryList: MutableList<MutablePair<Category, List<Product>?>>? = null
@@ -28,6 +30,11 @@ class HomeViewModel @Inject constructor(
     fun loadCategories() = categoryApi.callApi()
 
     fun loadMoreCategories(loadMoreCount: Int) = categoryApi.loadMoreCategories(loadMoreCount)
+
+    suspend fun fetchCategories(): Result<List<Category>> {
+        return getAndSaveCategoriesUseCase.execute(GetAndSaveCategoriesUseCase.ReqParams(true))
+    }
+
 
     /**
      * Loads the category and products from categoryPair to database
